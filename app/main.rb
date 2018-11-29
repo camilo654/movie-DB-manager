7793239c70e85b1d7a32dfc78d051afbb726567b
Christian Torres <cctorresr@unal.edu.co>
2018-11-29 16:33:36 -0500

    Se agrega busqueda en BD

--- app/main.rb
+++ app/main.rb
@@ -1,12 +1,8 @@
-require 'active_record'
+load 'DBManager.rb'
 require_relative './models/movie'
 
-def db_configuration
-  db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
-  YAML.load(File.read(db_configuration_file))
-end
-
-ActiveRecord::Base.establish_connection(db_configuration["development"])
+dbManager = DBManager.new
+dbManager.establish_connection
 
 print "Give me the title of the movie: "
 title = gets.chomp
@@ -14,8 +10,14 @@ title = gets.chomp
 print "Give me the director of the movie: "
 director = gets.chomp
 
-title = Movie.new(title: title, director: director)
-title.save!
+movie = Movie.new(title: title, director: director)
+movie.save!
+puts "Saved movie!"
+
+busqueda = Movie.find_by title: title
+
+puts "title: #{busqueda.title}"
+puts "director: #{busqueda.director}"
 
 puts "Number of movies in your database: #{Movie.count}"
 puts "Bye!"
